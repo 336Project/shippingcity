44ac6544e656f12d98ec0983de91c8d2ed125fa0
1220662994@qq.com <1220662994@qq.com>
2015-03-27 21:22:17 +0800

    2015-03-27 Helen 个人信息照片选择

--- shippingcity/src/com/ateam/shippingcity/activity/PersonalInfoActivity.java
+++ shippingcity/src/com/ateam/shippingcity/activity/PersonalInfoActivity.java
@@ -15,6 +15,11 @@ import android.widget.TextView;
  * @TODO 个人信息
  */
 public class PersonalInfoActivity extends HBaseActivity implements OnClickListener{
+	public static final int REQ_CODE_P=1000;
+	public static final int REQ_CODE_I=1001;
+	public static final int REQ_CODE_W=1002;
+	public static final int REQ_CODE_B=1003;
+	
 	private TextView mTxtUserName;
 	private TextView mTxtMobile;
 	@Override
@@ -28,7 +33,10 @@ public class PersonalInfoActivity extends HBaseActivity implements OnClickListen
 	private void initView() {
 		findViewById(R.id.layout_modify_mobile).setOnClickListener(this);
 		findViewById(R.id.layout_modify_name).setOnClickListener(this);
-		findViewById(R.id.layout_portrait).setOnClickListener(this);
+		findViewById(R.id.layout_pick_portrait).setOnClickListener(this);
+		findViewById(R.id.layout_pick_business_licence).setOnClickListener(this);
+		findViewById(R.id.layout_pick_IDCard).setOnClickListener(this);
+		findViewById(R.id.layout_pick_work_card).setOnClickListener(this);
 		mTxtUserName=(TextView) findViewById(R.id.txt_username);
 		mTxtMobile=(TextView) findViewById(R.id.txt_mobile);
 		findViewById(R.id.txt_modify_password).setOnClickListener(this);
@@ -47,8 +55,17 @@ public class PersonalInfoActivity extends HBaseActivity implements OnClickListen
 			b.putString("name", mTxtUserName.getText().toString());
 			jump(this, PersonalModifyNameActivity.class,b);
 			break;
-		case R.id.layout_portrait://修改头像
-			startActivityForResult(new Intent(this, PictureSelectDialogActivity.class), 1000);
+		case R.id.layout_pick_portrait://修改头像
+			startActivityForResult(new Intent(this, PictureSelectDialogActivity.class), REQ_CODE_P);
+			break;
+		case R.id.layout_pick_IDCard://修改身份证
+			startActivityForResult(new Intent(this, PictureSelectDialogActivity.class), REQ_CODE_I);
+			break;
+		case R.id.layout_pick_work_card://修改工牌
+			startActivityForResult(new Intent(this, PictureSelectDialogActivity.class), REQ_CODE_W);
+			break;
+		case R.id.layout_pick_business_licence://修改营业执照
+			startActivityForResult(new Intent(this, PictureSelectDialogActivity.class), REQ_CODE_B);
 			break;
 		case R.id.txt_modify_password://修改密码
 			jump(this, PersonalModifyPasswordActivity.class);
@@ -61,7 +78,16 @@ public class PersonalInfoActivity extends HBaseActivity implements OnClickListen
 	@Override
 	protected void onActivityResult(int requestCode , int resultCode , Intent data) {
 		if(resultCode==RESULT_OK){
-			
+			if(requestCode==REQ_CODE_P){//头像
+				
+			}else if(requestCode==REQ_CODE_I){//身份证
+				
+			}else if(requestCode==REQ_CODE_W){//工牌
+				
+			}else if(requestCode==REQ_CODE_B){//营业执照
+				
+			}
+			System.out.println("uri----"+data.getData().toString());
 		}
 	}
 }
--- shippingcity/src/com/ateam/shippingcity/activity/PictureSelectDialogActivity.java
+++ shippingcity/src/com/ateam/shippingcity/activity/PictureSelectDialogActivity.java
@@ -3,11 +3,15 @@ package com.ateam.shippingcity.activity;
 import com.ateam.shippingcity.R;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager;
+import android.widget.Toast;
 /**
  * 
  * @author 李晓伟
@@ -15,7 +19,8 @@ import android.view.WindowManager;
  * @TODO 图片选择弹窗
  */
 public class PictureSelectDialogActivity extends Activity implements OnClickListener{
-	
+	public static final int REQUEST_CODE_PICK_IMAGE=1001;
+	public static final int REQUEST_CODE_CAMERA=1002;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -31,10 +36,15 @@ public class PictureSelectDialogActivity extends Activity implements OnClickList
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.txt_take_piuture://拍照
-			
+            startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE), REQUEST_CODE_CAMERA);
 			break;
 		case R.id.txt_select_picture://选择照片
-			
+//			Intent intent = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);  
+//			startActivityForResult(intent,REQUEST_CODE_PICK_IMAGE);
+			Intent intent = new Intent(Intent.ACTION_GET_CONTENT);  
+			//intent.addCategory(Intent.CATEGORY_OPENABLE);  
+            intent.setType("image/*");
+            startActivityForResult(Intent.createChooser(intent, "选择图片"), REQUEST_CODE_PICK_IMAGE);
 			break;
 		case R.id.txt_cancel://取消
 			setResult(RESULT_CANCELED);
@@ -43,6 +53,27 @@ public class PictureSelectDialogActivity extends Activity implements OnClickList
 
 		default:
 			break;
+		}
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		Uri uri=null;
+		if (resultCode == RESULT_OK){
+			if(requestCode==REQUEST_CODE_PICK_IMAGE){
+				uri=data.getData();
+			}else if(requestCode==REQUEST_CODE_CAMERA){
+				Bundle bundle = data.getExtras();  
+                uri = (Uri) bundle.get(MediaStore.EXTRA_OUTPUT);
+			}
+		}
+		if(uri!=null){
+			Intent intent=new Intent();
+			intent.setData(uri);
+			setResult(RESULT_OK, data);
+			finish();
+		}else{
+			Toast.makeText(this, "获取图片失败，请选择一张图片", Toast.LENGTH_SHORT).show();
 		}
 	}
 }
