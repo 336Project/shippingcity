84c536fadfb728c0309eca718775c5d4ae803264
kk <Administrator@NDUVDZGBCV2OET0>
2015-03-28 11:14:41 +0800

    2015年3月28日11:14:30 晓康 更新其他2个片段

--- shippingcity/src/com/ateam/shippingcity/fragment/MyQuoteFragment.java
+++ shippingcity/src/com/ateam/shippingcity/fragment/MyQuoteFragment.java
@@ -1,12 +1,14 @@
 package com.ateam.shippingcity.fragment;
 
 import com.ateam.shippingcity.R;
+import com.ateam.shippingcity.activity.PalletAndQuoteCommonActivity;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 /**
  * 我的进价片段
  * @author Administrator
@@ -17,6 +19,11 @@ public class MyQuoteFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_my_quote, null);
+		PalletAndQuoteCommonActivity palletAndQuoteCommonActivity = (PalletAndQuoteCommonActivity) getActivity();
+		palletAndQuoteCommonActivity.getRightIcon().setVisibility(View.VISIBLE);
+		View inflate = inflater.inflate(R.layout.fragment_my_quote, null);
+		FrameLayout fl_my_quote = (FrameLayout) inflate.findViewById(R.id.fl_my_quote);
+		
+		return inflate;
 	}
 }
