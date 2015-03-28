2f7633cbcbcc56191f848d1dc276cd1bf005332c
kk <Administrator@NDUVDZGBCV2OET0>
2015-03-28 10:25:49 +0800

    2015年3月28日10:25:34 晓康 更新 我的进价部分片段

--- shippingcity/src/com/ateam/shippingcity/fragment/MyQuoteSeaTransportationFragment.java
+++ shippingcity/src/com/ateam/shippingcity/fragment/MyQuoteSeaTransportationFragment.java
@@ -7,16 +7,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 /**
- * 我的进价片段
+ * 我的进价-海运片段
  * @author Administrator
  *
  */
-public class MyQuoteFragment extends Fragment {
-	
+public class MyQuoteSeaTransportationFragment extends Fragment{
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_my_quote, null);
+		View inflate = inflater.inflate(R.layout.fragment_my_quote_sea_transport, null);
+		return inflate;
 	}
+
 }
