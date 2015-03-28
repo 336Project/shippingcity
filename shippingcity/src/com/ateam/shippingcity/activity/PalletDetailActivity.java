7e74b562bb05f13e9d3131193bc5b7c079384de2
Administrator <Administrator@PC201501121006>
2015-03-28 14:48:16 +0800

    2015-3-28 wtw 海运详情界面

---
+++ shippingcity/src/com/ateam/shippingcity/activity/PalletDetailActivity.java
@@ -0,0 +1,18 @@
+package com.ateam.shippingcity.activity;
+
+import com.ateam.shippingcity.R;
+import com.ateam.shippingcity.R.layout;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class PalletDetailActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_pallet_detail);
+	}
+
+}
