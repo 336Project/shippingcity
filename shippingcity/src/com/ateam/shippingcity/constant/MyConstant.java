35191daf60bda5cbe27fda979826a4df8eab3b5b
Administrator <Administrator@PC201501121006>
2015-03-31 14:32:57 +0800

    2015-3-31 wtw 图片查看实现

---
+++ shippingcity/src/com/ateam/shippingcity/constant/MyConstant.java
@@ -0,0 +1,12 @@
+package com.ateam.shippingcity.constant;
+
+/**
+ * 静态变量
+ * @version 
+ * @create_date 2015-3-31上午11:39:46
+ */
+public class MyConstant {
+
+	public static final String EXTRA_IMAGE_INDEX = "image_index";
+	public static final String EXTRA_IMAGE_URLS = "image_urls";
+}
