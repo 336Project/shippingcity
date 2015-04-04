1ab48355cf29eab9029e2f766a8bc8d56f502aaa
kk <Administrator@NDUVDZGBCV2OET0>
2015-04-04 12:51:10 +0800

    2015年4月4日12:51:02 晓康 部分图标

---
+++ shippingcity/src/com/ateam/shippingcity/model/MyQuoteToConfirmDetail.java
@@ -0,0 +1,13 @@
+package com.ateam.shippingcity.model;
+
+import java.io.Serializable;
+
+public class MyQuoteToConfirmDetail implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+}
